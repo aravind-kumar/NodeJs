c0f0cd89c9c033041de459edb550ef34cdefc41b
aravind-kumar <karavindkumar1993@gmail.com>
2018-01-02 19:26:55 -0500

    adding object factory

--- modeule2.js
+++ modeule2.js
@@ -1,5 +1,5 @@
 var modules = require("./modules"); //Care must be taken to not include the file extenstion
-require("./modules3");
+var modules3 = require("./modules3");
 // By importing modules3 we are in effect taking all the changes that are made in module where we are chaing the fav food.
 // Hence the log would also print favFood as pizza rather than icecream
 // This is because all the objects are refference by default and if want to new copies of the imported statement then we have to create an object factory
@@ -7,3 +7,12 @@ modules.printHelloWorld();
 modules.printHelloNortheastern();
 
 console.log("Main fav food is " + modules.favFood);
+
+
+//If however we want modules to be returned as different object everytime we have to use object factories.
+// Object factories are similar to module exports but they return an function object everytime they are called and we also 
+
+
+modules3 = modules3();
+
+console.log(modules3);
--- modules3.js
+++ modules3.js
@@ -3,3 +3,11 @@ var modules = require("./modules");
 modules.favFood = "Pizza";
 
 console.log("My fav food is " + modules.favFood);
+
+module.exports = function() {
+   return {
+      name: "Aravind",
+      lastName: "Kumar"
+   }
+
+};
